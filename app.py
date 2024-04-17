bd7020874e032e93c6055cafe377088cdfe9d667
pavankumarlbrce <142905971+pavankumarlbrce@users.noreply.github.com>
2024-04-17 11:09:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+import streamlit as st
+
+
+file = open("keys/.api_key")
+key=file.read()
+
+client=OpenAI(api_key=key)
+
+st.title("💬📝AI Code Reviewer🗒️🗨️")
+st.subheader("Find the errors in your code and get the correct code.")
+
+
+prompt=st.text_area("Enter code to check")
+
+if st.button("Review the code")==True:
+    response = client.chat.completions.create(
+
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a debugger of python code. Find out the errors in the python code and display the error and correction of code in a userfriendly manner"},
+            {"role": "user", "content": prompt}
+        ]
+    )
+    st.write(response.choices[0].message.content)
